30682a1ae39702431dc002e73a87a7fcf800c4d9
Boyan <Boyan.Digital@gmail.com>
2016-05-28 01:34:35 +0300

    First Steps in Coding [Exercises]

---
+++ 1. First Steps in Coding/4. Triangle of 55 Stars/TriangleOf55Stars.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class TriangleOf55Stars
+{
+    static void Main()
+    {
+        int rows = 10;
+        int starsCount = 0;
+
+        for (int row = 0; row <= rows; row++)
+        {
+            for (int col = 0; col < row; col++)
+            {
+                Console.Write("*");
+                starsCount++;
+            }
+            Console.WriteLine();
+        }
+        // Console.WriteLine(starsCount);
+    }
+}
---
+++ 1. First Steps in Coding/6. Square of Stars/SquareOfStars.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class SquareOfStars
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        char star = '*';
+        char space = ' ';
+
+        for (int col = 1; col <= n; col++)
+        {
+            for (int row = 1; row <= n; row++)
+            {
+                if ((row == 1 || row == n) || (col == 1 || col == n))
+                {
+                    Console.Write(star);
+                }
+                else
+                {
+                    Console.Write(space);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
