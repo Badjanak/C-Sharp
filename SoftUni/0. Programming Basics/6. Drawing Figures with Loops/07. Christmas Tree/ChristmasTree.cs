ece0a123434f2cdde326ec406d75b6b46075e014
Boyan <Boyan.Digital@gmail.com>
2016-06-06 21:45:18 +0300

    Drawing Figures with Loops [Exercises]

--- SoftUni/0. Programming Basics/6. Drawing Figures with Loops/07. Christmas Tree/ChristmasTree.cs
+++ SoftUni/0. Programming Basics/6. Drawing Figures with Loops/07. Christmas Tree/ChristmasTree.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 
-    class ChristmasTree
+class ChristmasTree
+{
+    static void Main()
     {
-        static void Main()
-        {
-        int n = int.Parse(Console.ReadLine());    
+        int n = int.Parse(Console.ReadLine());
+        char branch = '*';
+        string stem = " | ";
+        char space = ' ';
 
+        for (int i = 0; i <= n; i++)
+        {
+            Console.WriteLine(
+                new string(space, n - i) + new string(branch, i) +
+                stem +
+                new string(branch, i) + new string(space, n - i)
+                );
         }
     }
+}
