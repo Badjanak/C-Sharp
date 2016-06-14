fc683ea80dd9c07421446ac4ad6700ac2df27d52
Boyan Todorov <boyan.digital@gmail.com>
2016-06-14 12:12:49 +0300

    Drawing Figures with Loops [Exercises]

---
+++ SoftUni/0. Programming Basics/6. Drawing Figures with Loops/09. House/House.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+class House
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        char fill = '*';
+        char wall = '|';
+        char space = '-';
+        StringBuilder house = new StringBuilder();
+
+        int roofTop = 1;
+        bool isEven = n % 2 == 0;
+        if (isEven)
+        {
+            roofTop = 2;
+        }
+        // Roof
+        for (int i = roofTop; i < n + 1 / 2; i += 2)
+        {
+            house.Append(space, (n - i) / 2);
+            house.Append(fill, i);
+            house.Append(space, (n - i) / 2);
+            house.Append("\n");
+        }
+        // Middle
+        house.Append(fill, n);
+        house.Append("\n");
+        // Bottom
+        for (int i = 0; i < n / 2; i++)
+        {
+            house.Append(wall, 1);
+            house.Append(fill, n - 2);
+            house.Append(wall, 1);
+            house.Append("\n");
+        }
+
+        Console.WriteLine(house);
+    }
+}
---
+++ SoftUni/0. Programming Basics/6. Drawing Figures with Loops/10. Diamond/Diamond.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+class Diamond
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        char wall = '*';
+        char space = '-';
+        StringBuilder diamond = new StringBuilder();
+
+        int top = 1;
+        bool isEven = n % 2 == 0;
+        if (isEven)
+        {
+            top = 2;
+        }
+
+
+    }
+}
