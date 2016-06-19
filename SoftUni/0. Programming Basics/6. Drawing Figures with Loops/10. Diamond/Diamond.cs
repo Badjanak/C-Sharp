e00caf33efd192326c37a6b38b324143dd7e26c7
Boyan Todorov <boyan.digital@gmail.com>
2016-06-19 21:43:33 +0300

    Drawing Figures with Loops [Exercises]

--- SoftUni/0. Programming Basics/6. Drawing Figures with Loops/10. Diamond/Diamond.cs
+++ SoftUni/0. Programming Basics/6. Drawing Figures with Loops/10. Diamond/Diamond.cs
@@ -10,7 +10,34 @@ class Diamond
         char wall = '*';
         char space = '-';
         StringBuilder diamond = new StringBuilder();
-        
+
+        bool isEven = n % 2 == 0;
+        int start = 1;
+        if (isEven)
+        {
+            start = 2;
+        }
+        int inner = start;
+        int mid = (n - 1) / 2;
+
+        if (n < 3)
+        {
+            diamond.Append(wall, start);
+        }
+        else
+        {
+            diamond.Append(space, mid).Append(wall, start).Append(space, mid).Append("\n");
+            for (int outer = mid - 1; outer >= 0; outer--, inner += 2)
+            {
+                diamond.Append(space, outer).Append(wall, 1).Append(space, inner).Append(wall, 1).Append(space, outer).Append("\n");
+            }
+            inner -= 4;
+            for (int outer = 1; outer < mid; outer++, inner -= 2)
+            {
+                diamond.Append(space, outer).Append(wall, 1).Append(space, inner).Append(wall, 1).Append(space, outer).Append("\n");
+            }
+            diamond.Append(space, mid).Append(wall, start).Append(space, mid);
+        }
         Console.WriteLine(diamond);
     }
 }
