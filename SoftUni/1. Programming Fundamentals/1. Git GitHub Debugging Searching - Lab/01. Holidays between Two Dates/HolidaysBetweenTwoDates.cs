9879a84e0e43465230553d01866703a731430aa4
Boyan Todorov <boyan.digital@gmail.com>
2016-06-26 15:27:24 +0300

    Git GitHub Debugging Searching [Lab]

---
+++ SoftUni/1. Programming Fundamentals/1. Git GitHub Debugging Searching - Lab/01. Holidays between Two Dates/HolidaysBetweenTwoDates.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+class HolidaysBetweenTwoDates
+{
+    static void Main()
+    {
+        string dateFormat = "d.M.yyyy";
+        DateTime startDate = DateTime.ParseExact(Console.ReadLine(),
+            dateFormat, CultureInfo.InvariantCulture);
+        DateTime endDate = DateTime.ParseExact(Console.ReadLine(),
+            dateFormat, CultureInfo.InvariantCulture);
+        int holidaysCount = 0;
+
+        for (var date = startDate; date <= endDate; date = date.AddDays(1))
+        {
+            // Console.WriteLine(date);
+            if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday)
+            {
+                // Console.WriteLine(date.DayOfWeek);
+                holidaysCount++;
+            }
+        }
+
+        Console.WriteLine(holidaysCount);
+    }
+}
