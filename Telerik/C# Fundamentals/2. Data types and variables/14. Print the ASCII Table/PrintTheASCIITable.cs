171f7f0e6d5b7621cd64eaf9e0671cf8b80e1880
Boyan Todorov <boyan.digital@gmail.com>
2016-08-13 14:35:49 +0300

    Data types and variables [Homework]

---
+++ Telerik/C# Fundamentals/2. Data types and variables/14. Print the ASCII Table/PrintTheASCIITable.cs
@@ -0,0 +1,19 @@
+﻿/*
+ Write a program that prints the visible characters of the ASCII table on the console (characters from 33 to 126 including)
+ */
+
+using System;
+
+class PrintTheASCIITable
+{
+    static void Main()
+    {
+        for (int code = 33; code <= 126; code++)
+        {
+            char symbol = (char)code;
+            Console.Write($"{symbol}");
+        }
+    }
+}
+
+// http://bgcoder.com/Contests/Practice/Index/313#2
