062dd6347e8d46b457c6c4b06f810b86496ed967
Boyan Todorov <boyan.digital@gmail.com>
2016-08-13 14:11:51 +0300

    Introduction to Programming [Homework]

---
+++ Telerik/C# Fundamentals/1. Introduction to Programming/16. Long Sequence/LongSequence.cs
@@ -0,0 +1,28 @@
+﻿/*
+ Write a program that prints the first 1000 members of the sequence: 2, -3, 4, -5, 6, -7, …
+ */
+
+using System;
+
+class LongSequence
+{
+    static void Main()
+    {
+        int start = 2;
+        int members = 1000;
+
+        for (int i = start; i < start + members; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+            }
+            else
+            {
+                Console.WriteLine(-i);
+            }
+        }
+    }
+}
+
+// http://bgcoder.com/Contests/Practice/Index/314#5
