2a8edd4ae9324f15521186fa64f3b4f3b97d4744
Boyan Todorov <boyan.digital@gmail.com>
2016-08-15 13:28:28 +0300

    Introduction to Programming [Homework]

---
+++ Telerik/C# Fundamentals/1. Introduction to Programming/05. Print Name/PrintName.cs
@@ -0,0 +1,13 @@
+﻿/*
+ Create a C# application that print your name on the console.
+ */
+
+using System;
+
+class PrintName
+{
+    static void Main()
+    {
+        Console.WriteLine("Boyan");
+    }
+}
