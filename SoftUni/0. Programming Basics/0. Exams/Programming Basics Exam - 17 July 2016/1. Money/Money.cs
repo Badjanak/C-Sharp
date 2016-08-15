55e88ca1863d2a453096ea6d3d6c8c87a445463e
Boyan Todorov <boyan.digital@gmail.com>
2016-08-15 15:27:40 +0300

    Programming Basics Exam - 17 July 2016 [Solution]

---
+++ SoftUni/0. Programming Basics/0. Exams/Programming Basics Exam - 17 July 2016/1. Money/Money.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Money
+{
+    static void Main()
+    {
+        int bitCoins = int.Parse(Console.ReadLine());
+        double yuan = double.Parse(Console.ReadLine());
+        double commission = double.Parse(Console.ReadLine());
+
+        double sum = (bitCoins * 1168 + yuan * 0.15 * 1.76) / 1.95;
+        double tax = sum * commission / 100;
+
+        Console.WriteLine(sum - tax);
+    }
+}
---
+++ SoftUni/0. Programming Basics/0. Exams/Programming Basics Exam - 17 July 2016/2. Harvest/Harvest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Harvest
+{
+    static void Main()
+    {
+        int vineYardArea = int.Parse(Console.ReadLine());
+        double vine = double.Parse(Console.ReadLine());
+        int wineNeeded = int.Parse(Console.ReadLine());
+        int workers = int.Parse(Console.ReadLine());
+
+        double wineProduced = vineYardArea * 40 / 100 * vine / 2.5;
+        if (wineProduced >= wineNeeded)
+        {
+            Console.WriteLine($"Good harvest this year! Total wine: {Math.Floor(wineProduced)} liters.");
+            Console.WriteLine($"{(int)wineProduced - (int)wineNeeded} liters left -> {Math.Ceiling((wineProduced - wineNeeded) / workers)} liters per person.");
+        }
+        else
+        {
+            Console.WriteLine($"It will be a tough winter! More {Math.Floor(wineNeeded - wineProduced)} liters wine needed.");
+        }
+    }
+}
