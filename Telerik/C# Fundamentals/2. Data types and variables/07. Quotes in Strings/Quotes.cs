9d9380677bdef7814d9421b81da1bf3dd2d0d8f9
Boyan Todorov <boyan.digital@gmail.com>
2016-09-14 22:34:46 +0300

    Data types and variables [Homework]

---
+++ Telerik/C# Fundamentals/2. Data types and variables/07. Quotes in Strings/Quotes.cs
@@ -0,0 +1,17 @@
+﻿/*
+ Write a program that outputs The "use" of quotations causes difficulties.
+ */
+
+using System;
+
+class Quotes
+{
+    static void Main()
+    {
+        string escaped = "The \"use\" of quotations causes difficulties.";
+        string quoted = @"The ""use"" of quotations causes difficulties.";
+
+        Console.WriteLine($"{escaped}");
+        // Console.WriteLine($"{quoted}");
+    }
+}
